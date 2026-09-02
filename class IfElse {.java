6fe71511d7d2274efcdac66e50293263e3ea9fb1
Gaanashri07-max <gaanashri25@gmail.com>
2026-09-02 18:52:51 +0530

    Add files via upload

---
+++ class IfElse {.java
@@ -0,0 +1,14 @@
+class IfElse {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+              
+            System.out.println("He is eligible to vote");
+        } else {
+            System.out.println("He is not eligible to vote");
+        }
+
+        System.out.println("Thank you");
+    }
+}
