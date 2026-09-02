a9abe9e53d65cc6fa793ac8a7d041503339c00d9
Gaanashri07-max <gaanashri25@gmail.com>
2026-09-02 18:50:37 +0530

    Add files via upload

---
+++ class Simply {.java
@@ -0,0 +1,10 @@
+class Simply {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x >= 20) {
+            System.out.println("x is greater than or equal to 20");
+        } else {
+            System.out.println("x is less than 20");
+        }
+    }
+}
