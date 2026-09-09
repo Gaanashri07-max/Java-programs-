44551115cb3c28340c85c2d55265acd95fa2e58c
Gaanashri07-max <gaanashri25@gmail.com>
2026-09-09 11:01:15 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        int num = 7584;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reverse = " + reverse);
+    }
+}
