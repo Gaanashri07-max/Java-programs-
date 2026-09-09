ff068062de10aba3fef32933261d0e4238318391
Gaanashri07-max <gaanashri25@gmail.com>
2026-09-09 11:00:36 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("8 x " + i + " = " + (8 * i));
+        }
+    }
+}
